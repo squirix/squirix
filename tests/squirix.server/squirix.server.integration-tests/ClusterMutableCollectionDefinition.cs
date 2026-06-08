e3e7a2896d32bccfe41bf6d49f0c901b5fba6e2b
Alexander Efremov <admin@squirix.io>
2026-06-08 22:15:55 +0700

    chore(tests): remove unused xUnit collection definitions (fixes #20)

--- tests/squirix.server/squirix.server.integration-tests/ClusterMutableCollectionDefinition.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace Squirix.Server.IntegrationTests;
-
-/// <summary>
-/// xUnit collection definition that serializes cluster-mutating integration tests.
-/// </summary>
-[CollectionDefinition(IntegrationTestCollections.ClusterMutable, DisableParallelization = true)]
-public sealed class ClusterMutableCollectionDefinition;
