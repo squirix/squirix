a422a1259e1c1a1c03fd3ebccb054b978fad5033
Alexander Efremov <admin@squirix.io>
2026-06-08 22:24:59 +0700

    chore(tests): remove unused xUnit collection definitions (fixes #20) (#27)

--- tests/squirix.server/squirix.server.integration-tests/DurabilityRetentionCollectionDefinition.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace Squirix.Server.IntegrationTests;
-
-/// <summary>
-/// xUnit collection definition that serializes durability retention integration tests.
-/// </summary>
-[CollectionDefinition(IntegrationTestCollections.DurabilityRetention, DisableParallelization = true)]
-public sealed class DurabilityRetentionCollectionDefinition;
--- tests/squirix.server/squirix.server.integration-tests/IntegrationTestCollections.cs
+++
@@ -1,7 +0,0 @@
-namespace Squirix.Server.IntegrationTests;
-
-internal static class IntegrationTestCollections
-{
-    internal const string ClusterMutable = "ClusterMutable";
-    internal const string DurabilityRetention = "DurabilityRetention";
-}
