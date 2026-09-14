9cd717c93aad747da2a316defc5e0bc02ae7c926
Alexander Efremov <admin@squirix.io>
2026-09-14 13:37:35 +0700

    chore(release): prepare 0.1.0-preview.8 (#589)

--- src/squirix.server.host/BuildMetadata.cs
+++ src/squirix.server.host/BuildMetadata.cs
@@ -3,5 +3,5 @@ namespace Squirix.Server.Host;
 internal static class BuildMetadata
 {
     /// <summary>Keep in sync with $(SquirixPackageVersion) in Directory.Build.props.</summary>
-    internal const string InformationalVersion = "0.1.0-preview.7";
+    internal const string InformationalVersion = "0.1.0-preview.8";
 }
